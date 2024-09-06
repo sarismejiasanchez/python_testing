116d3465a8884247c7f4e457ad69831efb50b1d3
sarismejiasanchez <93800332+sarismejiasanchez@users.noreply.github.com>
2024-09-06 23:37:35 +0000

    ¿Qué es el Testing en Software?

--- test.py
+++ test.py
@@ -1,12 +1,40 @@
 def calculate_total(products):
     total = 0
     for product in products:
-        total *= product["price"]
+        total += product["price"]
     return total
 
 def test_calculate_total_with_empty_list():
-    print("prueba")
+
     assert calculate_total([]) == 0
+
+def test_calculate_total_with_single_product():
+    products = [
+        {
+            "name": "Notebook",
+            "price": 100
+        }
+    ]
+
+    assert calculate_total(products) == 100
+
+def test_calculate_total_with_multiple_products():
+    products = [
+        {
+            "name": "Book",
+            "price": 20
+        },
+        {
+            "name": "Pen",
+            "price": 5
+        }
+    ]
+
+    assert calculate_total(products) == 25
+
 
 if __name__ == '__main__':
     test_calculate_total_with_empty_list()
+    test_calculate_total_with_single_product()
+    test_calculate_total_with_multiple_products()
+
